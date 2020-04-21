5c2fbe60a88677202d80c2b73f1e782659735a99
bandura <banduraweb@ukr.net>
2020-04-21 16:48:12 +0300

    Updates

---
+++ precache-manifest.d268e2e045169b05749c0b2aaa4e0c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f97e3154da5c117708a2157d06157a7",
+    "url": "/test-graphQL-client/index.html"
+  },
+  {
+    "revision": "747f4742e759c028ab19",
+    "url": "/test-graphQL-client/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "7bc2b77f2ccce73eafc2",
+    "url": "/test-graphQL-client/static/css/main.24275666.chunk.css"
+  },
+  {
+    "revision": "747f4742e759c028ab19",
+    "url": "/test-graphQL-client/static/js/2.78870bfb.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/test-graphQL-client/static/js/2.78870bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc2b77f2ccce73eafc2",
+    "url": "/test-graphQL-client/static/js/main.95df19c0.chunk.js"
+  },
+  {
+    "revision": "5b0c66a63e3a5a48ae56",
+    "url": "/test-graphQL-client/static/js/runtime-main.ffb94870.js"
+  }
+]);
